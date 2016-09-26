8758981bd4619efa4ff9d0eda6248eb2f3f2d74a
luchanso <oleg@loutchansky.com>
2016-09-27 01:57:41 +0300

    Remove logger and add buttons

--- js/app.js
+++ js/app.js
@@ -1,55 +1,5 @@
 let tor = global.tor;
 
-tor.events.on('progress', updateProgress.bind(this));
-tor.events.on('message', writeInLog.bind(this));
-
-function rederictOnPornHub() {
-  const delay = 1;
-  let timer = {
-    tick: 0
-  };
-
-  writeInLog(`Running in ${delay} sec...`);
-
-  setTimeout(() => {
-    window.location = 'http://pornhub.com';
-  }, delay * 1000);
-
-  timer.counter = setInterval(() => {
-    timer.tick++;
-
-    writeInLog(`Running in ${delay - timer.tick} sec...`);
-    if (timer.tick >= delay) {
-      clearInterval(timer.counter);
-    }
-  }, 1000);
-}
-
-function updateProgress(value) {
-  progressbar.set(value);
-  progressbar.text.textContent = 'Loading... ' + Math.round(value * 100) + '%';
-
-  if (value === 1) {
-    rederictOnPornHub();
-  }
-}
-
-/**
- * Write in page log
- * @param  {String} message Your message
- */
-function writeInLog(message) {
-  let paragraph = document.createElement('p');
-  let newLine = document.createElement('br');
-  let node = document.createTextNode('[' + new Date().toLocaleTimeString() + '] ' + message);
-
-  paragraph.appendChild(node);
-  paragraph.appendChild(newLine);
-
-  let log = document.getElementsByClassName('log')[0];
-  log.appendChild(paragraph);
-}
-
 let progressbar = new ProgressBar.Circle('.container', {
   strokeWidth: 2,
   color: '#FF9900',
@@ -63,3 +13,32 @@ let progressbar = new ProgressBar.Circle('.container', {
     value: 'Loading...',
   }
 });
+
+tor.events.on('progress', updateProgress.bind(this));
+tor.events.on('message', console.log.bind(this));
+
+addEvents();
+
+function rederictOnPornHub() {
+  window.location = 'http://pornhub.com';
+}
+
+function updateProgress(value) {
+  progressbar.set(value);
+  progressbar.text.textContent = 'Loading... ' + Math.round(value * 100) + '%';
+
+  if (value === 1) {
+    progressbar.text.textContent = 'Ready to start';
+    activeStartBtn();
+  }
+}
+
+function activeStartBtn() {
+  let btn = document.getElementsByClassName('btn-start')[0];
+  btn.attributes.removeNamedItem('disabled');
+}
+
+function addEvents() {
+  let btn = document.getElementsByClassName('btn-start')[0];
+  btn.onclick = rederictOnPornHub;
+}
