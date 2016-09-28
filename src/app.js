d90e367f549996abaa42b3996550d1e97b13bcd8
luchanso <oleg@loutchansky.com>
2016-09-28 22:23:40 +0300

    Add password system

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 console.log('Electron Run');
 
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const async = require('async')
 const tor = require('./tor')
-const pages = `file://${__dirname}/../public/pages`
+const storage = require('electron-json-storage')
+const pages = `file://${__dirname}/../public/pages/`
 
 let win
 
@@ -11,15 +12,28 @@ tor.events.on('ready', () => {
   createProxy()
 })
 
+ipcMain.on('password', setPassword)
+
+function setPassword(event, password) {
+  storage.set('password', {pass: password});
+}
+
 function createWindow(next) {
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
   win.setMenu(null);
 
   next();
 }
 
-function runApp(next) {
-  win.loadURL(`${pages}/index.html`)
+function runApp(isLocked, next) {
+  let page = 'index.html'
+
+  if (isLocked) page = 'password-page.html'
+
+  win.loadURL(pages + page)
   win.webContents.openDevTools()
   win.on('closed', () => {
     win = null
@@ -46,11 +60,16 @@ function initTor(next) {
   tor.start(next)
 }
 
+function checkPassword(next) {
+  storage.has('password', next)
+}
+
 function appIsReady() {
   initTor();
 
   async.waterfall([
     createWindow,
+    checkPassword,
     runApp
   ], (err) => {
     if (err) {
