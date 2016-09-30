08219ba3119a7e2efffd76bddd3c9f320ebe8b03
luchanso <oleg@loutchansky.com>
2016-09-30 21:56:57 +0300

    Remove trash

--- public/js/clear.js
+++
@@ -1,4 +0,0 @@
-if (window.require) window.nodeRequire = require;
-delete window.require;
-delete window.exports;
-delete window.module;
--- public/js/pass-master.js
+++
@@ -1,6 +0,0 @@
-const crypto = window.nodeRequire('crypto');
-
-function createHash(string) {
-  const hash = crypto.createHash('sha512');
-  return hash.update(string).digest('hex');
-}
