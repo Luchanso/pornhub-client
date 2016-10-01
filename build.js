06872e8665b5116d3f16d633547b75a9ae3b6c4c
luchanso <oleg@loutchansky.com>
2016-10-01 13:47:48 +0300

    Update build.js file

--- build.js
+++ build.js
@@ -1,24 +1,45 @@
-var NwBuilder = require('nw-builder');
-var nw = new NwBuilder({
-    files: [
-      'public/**/*',
-      'src/**/*',
-      'tor/**/*',
-      'package.json'
-    ],
-    platforms: ['win32', 'win64'],
-    flavor: 'normal',
-    appName: 'ph-client',
-    appVersion: '1.0.0',
-    buildDir: './build',
-    buildType: 'versioned'
-    zip: true
-});
+const fs = require('fs')
 
-nw.on('log',  console.log);
+const NwBuilder = require('nw-builder')
+const utilityFile = [
+  'remove-run.bat',
+  'remove.bat',
+  'ffmpeg.dll'
+]
+const options = {
+  files: [
+    'public/**/*',
+    'src/**/*',
+    'tor/**/*',
+    'index.html',
+    'package.json'
+  ],
+  platforms: ['win32', 'win64'],
+  flavor: 'sdk', //'normal',
+  appName: 'ph-client',
+  appVersion: '1.0.1',
+  buildDir: './build',
+  buildType: 'versioned',
+  zip: true
+}
+const nw = new NwBuilder(options)
 
-nw.build().then(function () {
-   console.log('all done!');
-}).catch(function (error) {
-    console.error(error);
-});
+nw.on('log', console.log)
+
+nw.build().then(function() {
+  if (options.platforms.includes('win32')) {
+    for (let file of utilityFile) {
+      console.log('Copy win32 ', file)
+      fs.createReadStream(file).pipe(fs.createWriteStream(`./build/${options.appName} - v${options.appVersion}/win32/${file}`))
+    }
+  }
+  if (options.platforms.includes('win64')) {
+    for (let file of utilityFile) {
+      console.log('Copy win64 ', file)
+      fs.createReadStream(file).pipe(fs.createWriteStream(`./build/${options.appName} - v${options.appVersion}/win32/${file}`))
+    }
+  }
+  console.log('All done!')
+}).catch(function(error) {
+  console.error(error)
+})
